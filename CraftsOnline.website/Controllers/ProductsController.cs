4a06f17637581fb524fba1f62d43b9a480d2a167
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-10-06 14:04:36 +0100

    improved web service API

---
+++ CraftsOnline.website/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using CraftsOnline.website.models;
+using CraftsOnline.website.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CraftsOnline.website.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService) 
+        { 
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
--- CraftsOnline.website/Startup.cs
+++ CraftsOnline.website/Startup.cs
@@ -53,12 +53,12 @@ namespace CraftsOnline.website
                 endpoints.MapControllers();
                 endpoints.MapBlazorHub();
 
-                endpoints.MapGet("/products", (context) =>
-                {
-                    var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
-                    var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
-                    return context.Response.WriteAsync(json);
-                });
+                //endpoints.MapGet("/products", (context) =>
+                //{
+                //    var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
+                //    var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
+                //    return context.Response.WriteAsync(json);
+                //});
             });
         }
     }
